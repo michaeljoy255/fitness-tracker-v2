4baabbc63e3d8ae66c43e86d768a995ba07a0d07
michaeljoy255 <kodiakchaos@gmail.com>
2019-10-12 21:37:40 -0400

    scaffolding html and other pieces of site

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-// Get Month / Day / Year for navbar -------------------------------------------
-function getCurrentDate(){
-    var date = new Date();
-    var year = date.getFullYear();
-    var month = date.getMonth() + 1;
-    var day = date.getDate();
+// Returns mm/dd/yyyy date string
+function getCurrentDate() {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
     return month + "/" + day + "/" + year; 
 };
 document.getElementById("currentDate").innerHTML = getCurrentDate();
+
+function dataToClipboard() {
+    /* Get the text field */
+    const copyText = document.getElementById("dataResults");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+};
+
+$('.navbar-nav>li>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
